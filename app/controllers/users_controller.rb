31da5b5fa7a1f15d397cb23503ec39321b312993
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-08 17:43:39 +0000

    show the flash message when user registration is success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @page_title = 'signup'
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
