ed8145c54a088c429138abbc0fafd5a93005cd5b
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-08 16:58:42 +0000

    write integration test code and write the save logic in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,28 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
     @page_title = @user.name
+    @user = User.find(params[:id])
   end
   
   def new
     @page_title = 'signup'
     @user = User.new
+  end
+  
+  def create
+    @page_title = 'signup'
+    @user = User.new(user_params)
+    if @user.save
+      render 'new'
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
