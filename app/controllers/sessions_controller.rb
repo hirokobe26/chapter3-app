16227827da20dd1f43da6ba30cf515f73d4c8a56
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-08 21:08:37 +0000

    add the logic to check user login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,14 @@ class SessionsController < ApplicationController
   def new
     @page_title = 'Log in'
   end
+  
+  def create
+    @page_title = ''
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+    else
+      flash[:danger] = 'invalid email/password combination'
+      render 'new'
+    end
+  end
 end
