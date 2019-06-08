64ed9fb15adbfc354ab4745f57b9de519e7266f0
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-08 09:30:30 +0000

    add the function to hash password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,9 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(
       name: "example User", 
-      email: "user@hoge.com"
+      email: "user@hoge.com",
+      password: "foobar",
+      password_confirmation: "foobar"
       )
   end
   
@@ -57,5 +59,15 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+  
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
