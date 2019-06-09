65d2f90bf82bfc5e7e76bd44bfab9744da278b66
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-09 00:21:37 +0000

    add session logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,10 @@ class SessionsController < ApplicationController
   
   def create
     @page_title = ''
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
       render 'new'
