3edb26626eef0026d49399aaee068c9a83fc1502
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-09 03:33:11 +0000

    add function for logged-in user page and crated fixture data with hash password

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,11 @@ module SessionsHelper
         session[:user_id] = user.id
     end
     
-    def currnt_user
+    def current_user
         @current_user ||= User.find_by(id: session[:user_id])
+    end
+    
+    def logged_in?
+        !current_user.nil?
     end
 end
