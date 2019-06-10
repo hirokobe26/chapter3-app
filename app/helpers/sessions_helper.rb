5c57b6c56b002ea282ada45e873c0e665ae459b0
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-10 15:05:22 +0000

    use sessions or cookies to login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,16 +4,24 @@ module SessionsHelper
     end
     
     def remember(user)
-        user.remember #DBに暗号化したremember_tokenを保存してる
-        cookies.permanent.signed[:user_id] = user.id
+        user.remember #DBに暗号化したremember_tokenをremember_digesに保存してる
+        cookies.permanent.signed[:user_id] = user.id　#.signed[:user_id]暗号化してる
         cookies.permanent[:remember_token] = user.remember_token
     end
     
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.signed[:user_id]) #.signed[:user_id]で暗号化を復元してる
+                user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
+        end
     end
     
-    def logged_in?
+    def logged_in? #application.html.erbで呼び出されている
         !current_user.nil?
     end
     
