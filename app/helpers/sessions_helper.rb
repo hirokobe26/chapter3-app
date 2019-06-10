47f8aa251607a2184144b40b4cd7eb09d2cb6a65
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-10 14:22:16 +0000

    prepare for advanced login. save remember token in DB and save hashed remember token in cokkies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
         session[:user_id] = user.id
     end
     
+    def remember(user)
+        user.remember #DBに暗号化したremember_tokenを保存してる
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+    
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
