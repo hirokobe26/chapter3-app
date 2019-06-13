e38dfc83889138a33f2b8de5f7ec9f7137093b24
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-13 22:37:27 +0000

    add function to check if the user is wrong or correct

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -33,4 +33,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name,  @user.name
     assert_equal email, @user.email
   end
+  
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
