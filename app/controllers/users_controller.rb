7cc8e8cb19fa9572d7cdc2993d8f8d87c60296ab
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-13 21:54:12 +0000

    add the function for users to forcufully login if he or she is not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -29,7 +30,7 @@ class UsersController < ApplicationController
   
   def update
     @page_title = 'update'
-    @user = User.find(params[:id]) #失敗した時にNilを返すのでここでチェックして、Ifにはいる
+    @user = User.find(params[:id]) #失敗した時にNilを返すのでここでチェックして、If分にはいる
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -42,5 +43,12 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 end
