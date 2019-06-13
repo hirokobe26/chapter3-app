0073cefb2fcc8295491a2bd31d901542801369af
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-13 22:48:14 +0000

    refactor the code to avoid error in the test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "unsuccessful edit" do
