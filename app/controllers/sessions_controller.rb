b9d3c42f5886a0c52dc98b14addffcf81fa1102e
Hiro Okuno <hiro_kobe_26@yahoo.com>
2019-06-15 08:03:43 +0000

    add friendly forward function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
   def create
     @page_title = ''
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password]) #入力された値とDB内のハッシュ化されたパスワードを付け合わせる
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'invalid email/password combination'
       render 'new'
